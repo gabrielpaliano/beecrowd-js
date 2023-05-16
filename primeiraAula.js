f31dd6b7f8b36217c0b13af7b18592d1b349b97c
Gabriel KaEgso Paliano <gabrielpaliano17@gmail.com>
2023-05-16 18:14:58 -0300

    Ensinamentos básicos do JS

--- primeiraAula.js
+++ primeiraAula.js
@@ -1,1 +1,11 @@
-console.log('Oi')
+let valor = 25;
+
+const retornaRaiz = (numero) => Math.sqrt(numero);
+
+console.log(retornaRaiz(valor));
+
+//Explicado o que é let, var e const. 
+//Explicando como criar uma função arrow =>
+//Explicado como funciona o JS
+//Criada uma conta no BeeCrowd
+
